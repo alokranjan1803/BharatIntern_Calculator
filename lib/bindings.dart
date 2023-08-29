d83a99670581eee925cebd8e95b17cae83b7fbd4
alokfighter1803 <alok1832003@gmail.com>
2023-08-30 00:16:04 +0530

    Bharat Intern Project 2

---
+++ lib/bindings.dart
@@ -0,0 +1,11 @@
+import 'package:calcmate/controller/calculate_controller.dart';
+import 'package:calcmate/controller/theme_controller.dart';
+import 'package:get/instance_manager.dart';
+
+class MyBindings implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => CalculateController());
+    Get.lazyPut(() => ThemeController());
+  }
+}
